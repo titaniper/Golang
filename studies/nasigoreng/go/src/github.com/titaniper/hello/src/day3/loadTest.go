3c1cfcbf842abe8e5fd5ed3513e48570716d565a
titaniper <flutter.kr@gmail.com>
2024-07-03 12:13:27 +0900

    docs: 스터디 범위, jmeter 추가

--- studies/nasigoreng/go/src/github.com/titaniper/hello/src/day3/loadTest.go
+++ studies/nasigoreng/go/src/github.com/titaniper/hello/src/day3/loadTest.go
@@ -2,6 +2,18 @@ package day3
 
 // - HTTP 웹서버(no DB) 부하 테스트 (with JMeter)
 // https://jmeter.apache.org/download_jmeter.cgi
+
+// wget https://downloads.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz
+// curl -O https://downloads.apache.org/jmeter/binaries/apache-jmeter-5.5.tgz
+
+// tar -xvzf apache-jmeter-5.5.tgz
+
+// sudo mv apache-jmeter-5.6.3 /usr/local/bin/jmeter
+
+// echo "export PATH=\$PATH:/usr/local/bin/jmeter/bin" >> ~/.zshrc
+// source ~/.zshrc
+
+// jmeter -v
 func Load() {
 
 }
